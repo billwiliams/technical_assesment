4acd9591f7c4da285dc4b4572e0f2ebed94ef446
Wilson Ndirangu <wndirangu@lastmile-networks.net>
2017-05-03 15:58:27 +0300

    calculate the changed vehicles

--- Vehicle.py
+++ Vehicle.py
@@ -34,30 +34,39 @@ class Vehicle:
         return collect_number_plates
 
     def number_of_vehicles(self, number_plate_a, number_plate_b):
-        characters = sorted(set(string.ascii_letters.lower()))
-
-        if number_plate_a == number_plate_b:
+        number_plate_a = number_plate_a.lower()
+        number_plate_b = number_plate_b.lower()
+        if number_plate_a.replace(" ", "") == number_plate_b.replace(" ", ""):
             return 0
         else:
+            characters = sorted(set(string.ascii_letters.lower()))
             # map characters to the numeric equivalent in integers
-            maped_characters = dict(zip(characters, [ord(character) % 32 for character in characters]))
+            mapped_characters = dict(zip(characters, [ord(character) % 32 for character in characters]))
+            # obtain the character and digit parts of the number plate
             number_plate_characters_a = self.plate_characters(number_plate_a)
             number_plate_characters_b = self.plate_characters(number_plate_b)
             number_plate_digits_a = self.plate_digits(number_plate_a)
             number_plate_digits_b = self.plate_digits(number_plate_b)
-
-            # number_plate_a_digits=re.findall("\d+", number_plate_a.lower)
-            list_a = [maped_characters[c] for c in number_plate_characters_a]
-            list_b = [maped_characters[c] for c in number_plate_characters_b]
+            # change the character part into number to compute changes
+            list_a = [mapped_characters[c] for c in number_plate_characters_a]
+            list_b = [mapped_characters[c] for c in number_plate_characters_b]
+            # get the difference in the number plates
             diff = [x1 - x2 for (x1, x2) in zip(list_a, list_b)]
+            # These are factors that we multiply with depending on the placement of the changed character
             multiplying_factors = [0, 675324, 25974, 999]
-            difm = [x1 * x2 for (x1, x2) in zip(diff, multiplying_factors)]
-            carsx = difm[3] - int(number_plate_digits_b) + int(number_plate_digits_a) - 1
-            number = difm[1] - difm[2] - carsx
-            return number, diff, list_a, list_b
+            # we multiply the difference with the factors to obtain the respective changes when we wiggle a character
+            character_change_values = [x1 * x2 for (x1, x2) in zip(diff, multiplying_factors)]
+            # we calculate the changes in the digit part and last character
+            digit_changes = character_change_values[3] - int(number_plate_digits_b) + int(number_plate_digits_a) - 1
+            number_of_cars = character_change_values[1] - character_change_values[2] - digit_changes
+            if number_of_cars < 1:
+                return -number_of_cars
+            return number_of_cars
 
-    def plate_characters(self, number_plate):
+    @staticmethod
+    def plate_characters(number_plate):
         return ''.join(re.findall("[a-zA-Z]+", number_plate, re.M | re.I))
 
-    def plate_digits(self, number_plate):
+    @staticmethod
+    def plate_digits(number_plate):
         return ''.join(re.findall("\d{3}", number_plate, re.M | re.I))
